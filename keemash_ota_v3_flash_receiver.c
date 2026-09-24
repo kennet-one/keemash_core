55edc8ae09f965fbd8cca6e728c1621796ca804d
kennet-one <kennet1one@gmail.com>
2026-09-24 18:13:04 +0200

    report partial OTA transfer progress

--- keemash_ota_v3_flash_receiver.c
+++ keemash_ota_v3_flash_receiver.c
@@ -514,7 +514,8 @@ void keemash_ota_v3_flash_receiver_status(
 	status->resumed = receiver->resumed;
 	if (!receiver->stream) return;
 	status->raw_offset = receiver->stream->raw_offset;
-	status->encoded_offset = receiver->stream->encoded_offset;
+	status->encoded_offset = receiver->stream->encoded_offset +
+		receiver->stream->block_encoded_offset;
 	status->next_block_index = receiver->stream->next_block_index;
 }
 
